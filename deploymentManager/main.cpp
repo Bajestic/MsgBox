1ffe68c9124d08af88cf5d523a1d047f3749dfee
root <root@DESKTOP-491DHUO>
2023-10-08 12:45:59 +0200

    init commit

---
+++ deploymentManager/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Service started\n";
+    return 0;
+}
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SRC_DIR=$(pwd)
+BUILD_DIR=../build
+
+# Add user aliases (my cmake 3.27 version too)
+shopt -s expand_aliases
+source ~/.bashrc
+
+# cmake 3.22 doesn't work (generate cmake files in src directory)
+cmake -B${BUILD_DIR} -S${SRC_DIR} ${SRC_DIR}/CMakeLists.txt
